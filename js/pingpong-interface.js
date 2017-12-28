bf7680f0e3c31211df2de9b62d1ee42dc8204b04
Tyler Bussanich <tbussanich@gmail.com>
2017-12-27 21:57:19 -0800

    add everything

---
+++ js/pingpong-interface.js
@@ -0,0 +1,13 @@
+var Whatever = require('./../js/pingpong.js').thingy;
+
+$(document).ready(function() {
+  $('#ping-pong-form').submit(function(event) {
+    event.preventDefault();
+    var goal = $('#goal').val();
+    var simpleCalculator = new Whatever("hot pink");
+    var output = simpleCalculator.pingPong(goal);
+    output.forEach(function(element) {
+      $('#solution').append("<li>" + element + "</li>");
+    });
+  });
+});
