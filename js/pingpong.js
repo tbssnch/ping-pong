60ae46843700ae7b518ed6a792c8ecd06a8d85e0
Tyler Bussanich <tbussanich@gmail.com>
2017-12-31 21:44:55 -0800

    add email form, concatination of files, and minification

--- js/pingpong.js
+++ js/pingpong.js
@@ -6,11 +6,11 @@ Calculator.prototype.pingPong = function(goal) {
   var output = [];
   for (var i = 1; i <= goal; i++) {
     if (i % 15 === 0) {
-      output.push("ping-pong");
+      output.push("YOU GOT THIS DUDE");
     } else if (i % 3 === 0) {
-      output.push("ping");
+      output.push("AYY, LOOK AT YOU GO");
     } else if (i % 5 === 0) {
-      output.push("pong");
+      output.push("GULP GULP GULP");
     } else  {
       output.push(i);
     }
---
+++ tmp/allConcat.js
@@ -0,0 +1,22 @@
+var Whatever = require('./../js/pingpong.js').thingy;
+
+$(document).ready(function() {
+  $('#ping-pong-form').submit(function(event) {
+    event.preventDefault();
+    var goal = $('#goal').val();
+    var simpleCalculator = new Whatever("hot pink");
+    var output = simpleCalculator.pingPong(goal);
+    output.forEach(function(element) {
+      $('#solution').append("<li>" + element + "</li>");
+    });
+  });
+});
+
+$(document).ready(function(){
+  $('#signup').submit(function(event){
+    event.preventDefault();
+    var email = $('#email').val();
+    $('#signup').hide();
+    $('#solution').prepend('<p>Thank you, ' + email + ' has been added to our list!</p>');
+  });
+});
